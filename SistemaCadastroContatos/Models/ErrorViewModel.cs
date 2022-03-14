2ce49dfba3772c9996f9ec1b065f636da7ccfabd
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-14 11:54:04 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaCadastroContatos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SistemaCadastroContatos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
