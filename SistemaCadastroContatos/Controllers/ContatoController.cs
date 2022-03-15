41f70085d30d123b2d871b7e2bb44256156d49de
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-15 15:17:12 -0300

    Criando controller de contato

---
+++ SistemaCadastroContatos/Controllers/ContatoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaCadastroContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
