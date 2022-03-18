8fc1522f2782c86caad1b91a9fc29671d57b3be8
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-18 18:46:57 -0300

    Adicionando service de Banco de Dados

--- SistemaCadastroContatos/Startup.cs
+++ SistemaCadastroContatos/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SistemaCadastroContatos.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +25,7 @@ namespace SistemaCadastroContatos
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddEntityFrameworkSqlServer().AddDbContext<BancoContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
