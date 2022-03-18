70199264b0b918083b93e02b6fa8c88eb9c1cc3e
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-18 17:57:14 -0300

    Revert "Adicionando direcionamento para View Criar e Apagar"
    This reverts commit 81528772f0cae04a1bc1c364c1403b7187bbf1d2.

--- SistemaCadastroContatos/Controllers/ContatoController.cs
+++ SistemaCadastroContatos/Controllers/ContatoController.cs
@@ -8,20 +8,5 @@ namespace SistemaCadastroContatos.Controllers
         {
             return View();
         }
-
-        public IActionResult Criar()
-        {
-            return View();
-        }
-
-        public IActionResult Editar()
-        {
-            return View();
-        }
-
-        public IActionResult ApagarConfirmacao()
-        {
-            return View();
-        }
     }
 }
