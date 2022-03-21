72c84e512be78355bb3761f8fb88df285892fcab
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-21 16:45:06 -0300

    Criando Endpoint e Metodo de Criar para Adicionar

--- SistemaCadastroContatos/Controllers/ContatoController.cs
+++ SistemaCadastroContatos/Controllers/ContatoController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SistemaCadastroContatos.Models;
+using SistemaCadastroContatos.Repositories;
 
 namespace SistemaCadastroContatos.Controllers
 {
     public class ContatoController : Controller
     {
+        private readonly IContatoRepository _contatoRepository;
+        public ContatoController(IContatoRepository contatoRepository)
+        {
+            _contatoRepository = contatoRepository;
+        }
         public IActionResult Index()
         {
             return View();
@@ -22,6 +29,13 @@ namespace SistemaCadastroContatos.Controllers
         public IActionResult ApagarConfirmacao()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(ContatoModel contato)
+        {
+            _contatoRepository.Adicionar(contato);
+            return RedirectToAction("Index");
         }
     }
 }
