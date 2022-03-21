a2652c654c472607639d69a103cc8c0c9daee09c
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-21 16:16:28 -0300

    Criando Modelagem dos dados Contatos

---
+++ SistemaCadastroContatos/Data/BancoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaCadastroContatos.Models;
+
+namespace SistemaCadastroContatos.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ContatoModel> Contatos { get; set; }
+    }
+}
