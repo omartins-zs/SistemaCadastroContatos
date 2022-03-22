98c2926ac9c3a9800b0ee0bd55ca2d4bddc0c20f
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-22 17:36:43 -0300

    Criando metodo de atualizar Contato

--- SistemaCadastroContatos/Repositories/IContatoRepository.cs
+++ SistemaCadastroContatos/Repositories/IContatoRepository.cs
@@ -8,5 +8,6 @@ namespace SistemaCadastroContatos.Repositories
         ContatoModel ListarPorId(int id);
         List<ContatoModel> BuscarTodos();
         ContatoModel Adicionar(ContatoModel contato);
+        ContatoModel Atualizar(ContatoModel contato);
     }
 }
