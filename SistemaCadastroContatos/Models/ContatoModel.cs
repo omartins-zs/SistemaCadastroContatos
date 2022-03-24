8c0618e343331454da0757a2bc339ca9528966fc
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-24 14:46:45 -0300

    Usando DataAnnotations para Required

--- SistemaCadastroContatos/Models/ContatoModel.cs
+++ SistemaCadastroContatos/Models/ContatoModel.cs
@@ -1,13 +1,18 @@
-﻿namespace SistemaCadastroContatos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaCadastroContatos.Models
 {
     public class ContatoModel
     {
         public int Id { get; set; }
 
+        [Required]
         public string Nome { get; set; }
 
+        [Required]
         public string Email { get; set; }
 
+        [Required]
         public string Celular { get; set; }
     }
 }
