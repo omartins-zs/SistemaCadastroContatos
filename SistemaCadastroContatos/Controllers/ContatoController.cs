cf2fb1b3278786c820785171cc63d5a43397f361
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-24 14:55:47 -0300

    Configurando Validaçoes de mensagem no Controller

--- SistemaCadastroContatos/Controllers/ContatoController.cs
+++ SistemaCadastroContatos/Controllers/ContatoController.cs
@@ -44,8 +44,12 @@ namespace SistemaCadastroContatos.Controllers
         [HttpPost]
         public IActionResult Criar(ContatoModel contato)
         {
-            _contatoRepository.Adicionar(contato);
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _contatoRepository.Adicionar(contato);
+                return RedirectToAction("Index");
+            }
+            return View(contato);
         }
 
         [HttpPost]
