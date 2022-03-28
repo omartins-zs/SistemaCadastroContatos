f3ac82408508b2b52ecb2c85fc3c02c2fe90e805
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-28 12:30:00 -0300

    Criando implementaçao de Usuario

---
+++ SistemaCadastroContatos/Repositories/UsuarioRepository.cs
@@ -0,0 +1,63 @@
+﻿using SistemaCadastroContatos.Data;
+using SistemaCadastroContatos.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SistemaCadastroContatos.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly BancoContext _bancoContext;
+        public UsuarioRepository(BancoContext bancoContext)
+        {
+            _bancoContext = bancoContext;
+        }
+        public UsuarioModel ListarPorId(int id)
+        {
+            return _bancoContext.Usuarios.FirstOrDefault(x => x.Id == id);
+
+        }
+
+        public List<UsuarioModel> BuscarTodos()
+        {
+            return _bancoContext.Usuarios.ToList();
+        }
+
+        public UsuarioModel Adicionar(UsuarioModel usuario)
+        {
+            //  Adicionar no banco de dados
+            _bancoContext.Usuarios.Add(usuario);
+            _bancoContext.SaveChanges();
+
+            return usuario;
+        }
+
+        public UsuarioModel Atualizar(UsuarioModel usuario)
+        {
+            UsuarioModel usuarioDB = ListarPorId(usuario.Id);
+
+            if (usuarioDB == null) throw new System.Exception("Houve um erro na atualizaçao do usuario");
+
+            usuarioDB.Nome = usuario.Nome;
+            usuarioDB.Email = usuario.Email;
+            usuarioDB.Login = usuario.Login;
+
+            _bancoContext.Usuarios.Update(usuarioDB);
+            _bancoContext.SaveChanges();
+
+            return usuarioDB;
+        }
+
+        public bool Apagar(int id)
+        {
+            UsuarioModel usuarioDB = ListarPorId(id);
+
+            if (usuarioDB == null) throw new System.Exception("Houve um erro na deleção do usuarios");
+
+            _bancoContext.Usuarios.Remove(usuarioDB);
+            _bancoContext.SaveChanges();
+
+            return true;
+        }
+    }
+}
