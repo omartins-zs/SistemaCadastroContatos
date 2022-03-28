1e733448a1285bc2d8edd9f61fc0da2c17d3cd44
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-28 10:59:45 -0300

    Modelando dados do Usuario

---
+++ SistemaCadastroContatos/Models/UsuarioModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SistemaCadastroContatos.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+    }
+}
