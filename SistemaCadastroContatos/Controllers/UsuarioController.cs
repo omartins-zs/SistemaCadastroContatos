de938f9fdba28e988562347b6241d741073ec17f
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-28 11:41:43 -0300

    Criando Usuario Controller

---
+++ SistemaCadastroContatos/Controllers/UsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaCadastroContatos.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
