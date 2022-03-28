f57182eac4d228fad47cb37f0f08bcf10d504177
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-28 12:10:10 -0300

    Criando Interface de UsuarioRepository

---
+++ SistemaCadastroContatos/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using SistemaCadastroContatos.Models;
+using System.Collections.Generic;
+
+namespace SistemaCadastroContatos.Repositories
+{
+    public interface IUsuarioRepository
+    {
+        List<UsuarioModel> BuscarTodos();
+        UsuarioModel ListarPorId(int id);
+        UsuarioModel Adicionar(UsuarioModel usuario);
+        UsuarioModel Atualizar(UsuarioModel usuario);
+        bool Apagar(int id);
+    }
+}
