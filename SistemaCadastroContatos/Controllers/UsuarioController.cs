986d924a58cf9a23df10398b906cbe42c7776193
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-28 12:42:06 -0300

    Add construct e injecao de dependencia no Usuario

--- SistemaCadastroContatos/Controllers/UsuarioController.cs
+++ SistemaCadastroContatos/Controllers/UsuarioController.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SistemaCadastroContatos.Repositories;
 
 namespace SistemaCadastroContatos.Controllers
 {
     public class UsuarioController : Controller
     {
+        private readonly IUsuarioRepository _usuarioRepository;
+        public UsuarioController(IUsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
         public IActionResult Index()
         {
             return View();
