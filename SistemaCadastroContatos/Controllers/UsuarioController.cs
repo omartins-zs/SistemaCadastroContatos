752c8a2f86c8f75490d1bd4714f146329e8fb1ea
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-28 12:47:50 -0300

    Ajustando Route Criar e POST de Criar

--- SistemaCadastroContatos/Controllers/UsuarioController.cs
+++ SistemaCadastroContatos/Controllers/UsuarioController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SistemaCadastroContatos.Models;
 using SistemaCadastroContatos.Repositories;
+using System.Collections.Generic;
 
 namespace SistemaCadastroContatos.Controllers
 {
@@ -12,7 +14,34 @@ namespace SistemaCadastroContatos.Controllers
         }
         public IActionResult Index()
         {
+            List<UsuarioModel> usuarios = _usuarioRepository.BuscarTodos();
+            return View(usuarios);
+        }
+
+        public IActionResult Criar()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(UsuarioModel usuario)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _usuarioRepository.Adicionar(usuario);
+                    TempData["MensagemSucesso"] = "Usuario cadastrado com sucesso";
+                    return RedirectToAction("Index");
+                }
+                return View(usuario);
+            }
+            catch (System.Exception erro)
+            {
+                // ' $ ' para poder concatenar na mensagem
+                TempData["MensagemErro"] = $"Ops, nao conseguimos cadastrar seu usuario, tente novamente, detalhe do erro: {erro.Message} ";
+                return RedirectToAction("Index");
+            }
         }
     }
 }
