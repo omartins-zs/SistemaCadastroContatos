50b0ba15c4a927567dc19003f2ac63b034c4d322
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-31 14:31:48 -0300

    Add validaçoes do Annotation no Usuario Model

--- SistemaCadastroContatos/Models/UsuarioModel.cs
+++ SistemaCadastroContatos/Models/UsuarioModel.cs
@@ -1,15 +1,26 @@
 ﻿using SistemaCadastroContatos.Enums;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace SistemaCadastroContatos.Models
 {
     public class UsuarioModel
     {
         public int Id { get; set; }
+       
+        [Required(ErrorMessage = "Digite o nome do usuario")]
         public string Nome { get; set; }
+        
+        [Required(ErrorMessage = "Digite o nome do usuario")]
         public string Login { get; set; }
+
+        [Required(ErrorMessage = "Digite o e-mail do usuario")]
+        [EmailAddress(ErrorMessage = "O e-mail informado não é valido!")]
         public string Email { get; set; }
+        
         public PerfilEnum Perfil { get; set; }
+
+        [Required(ErrorMessage = "Digite o senha do usuario")]
         public string Senha { get; set; }
         public DateTime DataCadastro { get; set; }
         public DateTime? DataAtualizacao { get; set; }
