a69ac7e2a72edb57ac58e3dd9822d6dd1fd46167
omartins-zs <gabrielmatheus209@gmail.com>
2022-04-12 19:26:49 -0300

    Alterando route padrão do Startup

--- SistemaCadastroContatos/Startup.cs
+++ SistemaCadastroContatos/Startup.cs
@@ -58,7 +58,7 @@ namespace SistemaCadastroContatos
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
