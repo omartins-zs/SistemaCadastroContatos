c09c33aa2379c7f71c07163f8706dd2a211ec073
omartins-zs <gabrielmatheus209@gmail.com>
2022-04-21 15:18:07 -0300

    Create class de implementaçao dos metodos Session

---
+++ SistemaCadastroContatos/Helper/Session.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using SistemaCadastroContatos.Models;
+
+namespace SistemaCadastroContatos.Helper
+{
+    public class Session : ISession
+    {
+        private readonly IHttpContextAccessor _httpContext;
+
+        public Session(IHttpContextAccessor httpContext)
+        {
+            _httpContext = httpContext;
+        }
+        public UsuarioModel BuscarSessionDoUsuario()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CriarSessionDoUsuario(UsuarioModel usuario)
+        {
+            string valor = JsonConvert.SerializeObject(usuario);
+            _httpContext.HttpContext.Session.SetString("sessionUsuarioLogado", valor) ;
+        }
+
+        public void RemoverSessionDoUsuario()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
