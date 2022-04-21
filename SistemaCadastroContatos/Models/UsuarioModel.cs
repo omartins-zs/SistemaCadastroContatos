edcdfda281e096275a4b17fa65013f4d260f4c49
omartins-zs <gabrielmatheus209@gmail.com>
2022-04-21 14:53:29 -0300

    Criando metodo SenhaValida

--- SistemaCadastroContatos/Models/UsuarioModel.cs
+++ SistemaCadastroContatos/Models/UsuarioModel.cs
@@ -26,5 +26,9 @@ namespace SistemaCadastroContatos.Models
         public DateTime DataCadastro { get; set; }
         public DateTime? DataAtualizacao { get; set; }
 
+        public bool SenhaValida(string senha)
+        {
+            return Senha == senha;
+        }
     }
 }
