e03e59f834e4a4bd58ed4ed7a4d7c80f880fcdd0
omartins-zs <gabrielmatheus209@gmail.com>
2022-04-21 15:10:46 -0300

    Criando Interface para criar e gerenciar session

---
+++ SistemaCadastroContatos/Helper/ISession.cs
@@ -0,0 +1,12 @@
+﻿using SistemaCadastroContatos.Models;
+
+namespace SistemaCadastroContatos.Helper
+{
+    public interface ISession
+    {
+        void CriarSessionDoUsuario(UsuarioModel usuario);
+
+        void RemoverSessionDoUsuario();
+        UsuarioModel BuscarSessionDoUsuario();
+    }
+}
