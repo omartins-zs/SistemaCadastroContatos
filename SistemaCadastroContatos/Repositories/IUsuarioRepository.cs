848abbd8c0197ca8b088dbe847650863a72c5a81
omartins-zs <gabrielmatheus209@gmail.com>
2022-04-21 14:46:34 -0300

    Criando metodo Buscar Por Login

--- SistemaCadastroContatos/Repositories/IUsuarioRepository.cs
+++ SistemaCadastroContatos/Repositories/IUsuarioRepository.cs
@@ -5,6 +5,7 @@ namespace SistemaCadastroContatos.Repositories
 {
     public interface IUsuarioRepository
     {
+        UsuarioModel BuscarPorLogin(string login);
         UsuarioModel ListarPorId(int id);
         List<UsuarioModel> BuscarTodos();
         UsuarioModel Adicionar(UsuarioModel usuario);
