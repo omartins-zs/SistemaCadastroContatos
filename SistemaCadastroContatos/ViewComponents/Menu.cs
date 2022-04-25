cba10025b06bfe6e4e67443e0f84808e979100fd
omartins-zs <gabrielmatheus209@gmail.com>
2022-04-25 13:37:04 -0300

    Criando Session do Usuario

---
+++ SistemaCadastroContatos/ViewComponents/Menu.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SistemaCadastroContatos.Models;
+using System.Threading.Tasks;
+
+namespace SistemaCadastroContatos.ViewComponents
+{
+    public class Menu : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            string sessaoUsuario = HttpContext.Session.GetString("sessionUsuarioLogado");
+
+            if (string.IsNullOrEmpty(sessaoUsuario)) return null;
+
+            UsuarioModel usuario = JsonConvert.DeserializeObject<UsuarioModel>(sessaoUsuario);
+
+            return View(usuario);
+        }
+    }
+}
