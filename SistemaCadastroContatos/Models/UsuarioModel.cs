747dbfd5e2128e49f3e8322291a01c0f087d55f6
Gabriel Martins <gabrielmatheus209@gmail.com>
2023-09-20 19:10:20 -0300

    Criando Metodo para GerarNovaSenha

--- SistemaCadastroContatos/Models/UsuarioModel.cs
+++ SistemaCadastroContatos/Models/UsuarioModel.cs
@@ -36,5 +36,13 @@ namespace SistemaCadastroContatos.Models
         {
             Senha = Senha.GerarHash();
         }
+
+        public string GerarNovaSenha()
+        {
+           string novaSenha = Guid.NewGuid().ToString().Substring(0, 8);
+            Senha = novaSenha.GerarHash();
+            return novaSenha;
+        }
+
     }
 }
