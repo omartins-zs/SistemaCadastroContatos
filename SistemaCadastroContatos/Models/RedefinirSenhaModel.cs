36c62e2743df59c32cca7f63d57283d1ef78fd0a
Gabriel Martins <gabrielmatheus209@gmail.com>
2023-09-20 18:34:16 -0300

    Criando Model para RedefinirSenha

---
+++ SistemaCadastroContatos/Models/RedefinirSenhaModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaCadastroContatos.Models
+{
+    public class RedefinirSenhaModel
+    {
+        [Required(ErrorMessage = "Digite o login")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Digite o email")]
+        public string Email { get; set; }
+    }
+}
