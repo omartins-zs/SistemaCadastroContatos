e69ccc935ac331b9befe3ab313065d033daac090
Gabriel Martins <gabrielmatheus209@gmail.com>
2023-09-20 17:01:37 -0300

    Create Metodo de Criptografia, GerarHash da Senha

---
+++ SistemaCadastroContatos/Helper/Criptografia.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Intrinsics.Arm;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SistemaCadastroContatos.Helper
+{
+    public static class Criptografia
+    {
+        /*  Metodo de EXTENSAO  */
+        public static string GerarHash(this string valor)
+        {
+            var hash = SHA1.Create();
+            var enconding = new ASCIIEncoding();
+            var array = enconding.GetBytes(valor);
+
+            var strHexa = new StringBuilder();
+
+            foreach (var item in array)
+            {
+                strHexa.Append(item.ToString("x2"));
+            }
+            return strHexa.ToString();
+        }
+    }
+}
