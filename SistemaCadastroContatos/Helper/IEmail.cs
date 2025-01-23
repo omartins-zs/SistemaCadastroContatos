db1d85b237dced495acc9b2a1f6b3ea167650b8e
omartins-zs <gabrielmatheus209@gmail.com>
2025-01-23 19:54:18 -0300

    Criando Interface de Email

---
+++ SistemaCadastroContatos/Helper/IEmail.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaCadastroContatos.Helper
+{
+    public interface IEmail
+    {
+        bool Enviar(string email, string assunto, string mensagem);
+    }
+}
