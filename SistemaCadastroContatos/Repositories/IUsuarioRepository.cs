0c4afe5a7d4962dee29ba4887f95ff727525f29a
omartins-zs <gabrielmatheus209@gmail.com>
2025-01-23 23:05:57 -0300

    Add BuscarId e AlterarSenha no IUsuarioRepository

--- SistemaCadastroContatos/Repositories/IUsuarioRepository.cs
+++ SistemaCadastroContatos/Repositories/IUsuarioRepository.cs
@@ -5,12 +5,13 @@ namespace SistemaCadastroContatos.Repositories
 {
     public interface IUsuarioRepository
     {
-        UsuarioModel BuscarPorLogin(string login);
-        UsuarioModel BuscarPorEmailELogin(string email,string login);
-        UsuarioModel ListarPorId(int id);
+          UsuarioModel BuscarPorLogin(string login);
+        UsuarioModel BuscarPorEmailELogin(string email, string login);
         List<UsuarioModel> BuscarTodos();
+        UsuarioModel BuscarPorID(int id);
         UsuarioModel Adicionar(UsuarioModel usuario);
         UsuarioModel Atualizar(UsuarioModel usuario);
-        bool Apagar(int id);
+        UsuarioModel AlterarSenha(AlterarSenhaModel alterarSenhaModel);
+        bool Apagar (int id);
     }
 }
