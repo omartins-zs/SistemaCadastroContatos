fe3ce005f99b3e974113f7b801da68dddeca7a50
omartins-zs <gabrielmatheus209@gmail.com>
2025-01-23 23:02:24 -0300

    Criando o Controller Alterar Senha e Fnc Alterar

---
+++ SistemaCadastroContatos/Controllers/AlterarSenhaController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SistemaCadastroContatos.Helper;
+using SistemaCadastroContatos.Models;
+using SistemaCadastroContatos.Repositories;
+
+namespace SistemaCadastroContatos.Controllers
+{
+    public class AlterarSenhaController : Controller
+    {
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+        private readonly ISessao _sessao;
+        public AlterarSenhaController(IUsuarioRepositorio usuarioRepositorio,
+                                      ISessao sessao)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+            _sessao = sessao;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Alterar(AlterarSenhaModel alterarSenhaModel)
+        {
+            try
+            {
+                UsuarioModel usuarioLogado = _sessao.BuscarSessaoDoUsuario();
+                alterarSenhaModel.Id = usuarioLogado.Id;
+                if (ModelState.IsValid)
+                {
+                    _usuarioRepositorio.AlterarSenha(alterarSenhaModel);
+                    TempData["MensagemSucesso"] = "Senha alterada com sucesso!";
+                    return View("Index", alterarSenhaModel);
+                }
+                return View("Index", alterarSenhaModel);
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos alterar sua senha, tente novamante, detalhe do erro: {erro.Message}";
+                return View("Index", alterarSenhaModel);
+            }
+        }
+    }
+}
