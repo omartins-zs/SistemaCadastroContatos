90d2646e8ae45c35bab3b9f692f674c89c3191ac
omartins-zs <gabrielmatheus209@gmail.com>
2025-01-23 23:06:20 -0300

    Ajuste no Buscar por Id e Create AlterarSenha

--- SistemaCadastroContatos/Repositories/UsuarioRepository.cs
+++ SistemaCadastroContatos/Repositories/UsuarioRepository.cs
@@ -1,8 +1,8 @@
 ﻿using SistemaCadastroContatos.Data;
 using SistemaCadastroContatos.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 
 namespace SistemaCadastroContatos.Repositories
 {
@@ -24,7 +24,7 @@ namespace SistemaCadastroContatos.Repositories
             return _bancoContext.Usuarios.FirstOrDefault(x => x.Email.ToUpper() == email.ToUpper() && x.Login.ToUpper() == login.ToUpper());
         }
 
-        public UsuarioModel ListarPorId(int id)
+        public UsuarioModel BuscarPorID(int id)
         {
             return _bancoContext.Usuarios.FirstOrDefault(x => x.Id == id);
 
@@ -48,7 +48,7 @@ namespace SistemaCadastroContatos.Repositories
 
         public UsuarioModel Atualizar(UsuarioModel usuario)
         {
-            UsuarioModel usuarioDB = ListarPorId(usuario.Id);
+            UsuarioModel usuarioDB = BuscarPorID(usuario.Id);
 
             if (usuarioDB == null) throw new System.Exception("Houve um erro na atualizaçao do usuario");
 
@@ -64,9 +64,29 @@ namespace SistemaCadastroContatos.Repositories
             return usuarioDB;
         }
 
+        public UsuarioModel AlterarSenha(AlterarSenhaModel alterarSenhaModel)
+        {
+            UsuarioModel usuarioDB = BuscarPorID(alterarSenhaModel.Id);
+
+            if (usuarioDB == null) throw new Exception("Houve um erro na atualização da senha, usuário não encontrado!");
+
+            if (!usuarioDB.SenhaValida(alterarSenhaModel.SenhaAtual)) throw new Exception("Senha atual não confere!");
+
+            if (usuarioDB.SenhaValida(alterarSenhaModel.NovaSenha)) throw new Exception("Nova senha deve ser diferente da senha atual!");
+
+            usuarioDB.SetNovaSenha(alterarSenhaModel.NovaSenha);
+            usuarioDB.DataAtualizacao = DateTime.Now;
+
+            _bancoContext.Usuarios.Update(usuarioDB);
+            _bancoContext.SaveChanges();
+
+            return usuarioDB;
+        }
+
+
         public bool Apagar(int id)
         {
-            UsuarioModel usuarioDB = ListarPorId(id);
+            UsuarioModel usuarioDB = BuscarPorID(id);
 
             if (usuarioDB == null) throw new System.Exception("Houve um erro na deleção do usuarios");
 
