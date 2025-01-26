4114867e17c64612fc73805a53592675e626f3d9
omartins-zs <gabrielmatheus209@gmail.com>
2025-01-26 16:37:22 -0300

    Mapeando Relacionamento Tabela de Contato

---
+++ SistemaCadastroContatos/Data/Map/ContatoMap.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SistemaCadastroContatos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaCadastroContatos.Data.Map
+{
+    public class ContatoMap : IEntityTypeConfiguration<ContatoModel>
+    {
+        public void Configure(EntityTypeBuilder<ContatoModel> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.HasOne(x => x.Usuario);
+        }
+    }
+}
